df0f41d091d16f36ceeb3615e1e066a4b1a749f0
omega <omega@gmail.com>
2024-12-13 13:06:47 -0800

    Added Basic Canvas Function

--- src/DrawingArea.js
+++ src/DrawingArea.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+const styles = {
+  border: '0.0625rem solid #9c9c9c',
+  borderRadius: '0.25rem',
+};
+
+const Canvas = () => {
+  return (
+    <ReactSketchCanvas
+      style={styles}
+      width="27em"
+      height="27em"
+      strokeWidth={4}
+      strokeColor="red"
+    />
+  );
+};
 
 function DrawingArea() {
-  return <div className="drawing">Drawing Area</div>;
+  return (
+  <div className="drawing">
+    <Canvas />
+  </div>
+  );
 }
 
 export default DrawingArea;
