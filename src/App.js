0cf2fd301a0dd690a18bd9b9d7a6e6fb0e3de96c
omega <omega@gmail.com>
2024-12-13 13:50:59 -0800

    Removed ToolButtons.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './App.css';
-import ToolButtons from "./ToolButtons";
 import DrawingArea from "./DrawingArea";
 import Output from "./Output";
 
 function App() {
   return (
     <div class="screen">
-      <ToolButtons />
       <DrawingArea />
       <Output />
     </div>
--- src/DrawingArea.js
+++ src/DrawingArea.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { ReactSketchCanvas } from 'react-sketch-canvas';
+import Button from "./Button";
+import Paintbrush from "./images/paintbrush.png";
+import Paintbucket from "./images/paintbucket.png";
+import Eraser from "./images/erase.png";
+import './App.css';
 
 const styles = {
   border: '0.0625rem solid #9c9c9c',
   borderRadius: '0.25rem',
 };
 
+const buttonsConfig = [
+  {
+    icon: <img src={Paintbrush} alt="Paintbrush" className="icon"/>,
+    label: "Paintbrush",
+    onClick: () => console.log("Paintbrush Clicked")
+  },
+  {
+    icon:  <img src={Paintbucket} alt="Paintbucket" className="icon"/>,
+    label: "Paintbucket",
+    onClick: () => console.log("Paintbucket Clicked")
+  },
+  {
+    icon:  <img src={Eraser} alt="Erase" className="icon"/>,
+    label: "Eraser",
+    onClick: () => console.log("Eraser Clicked")
+  }
+]
+
 const Canvas = () => {
   return (
-    <ReactSketchCanvas
-      style={styles}
-      width="27em"
-      height="27em"
-      strokeWidth={4}
-      strokeColor="red"
-    />
+    <><div className="tool-buttons">
+      {buttonsConfig.map((button, index) => <Button
+        key={index}
+        icon={button.icon}
+        onClick={button.onClick} />
+      )}
+    </div><ReactSketchCanvas
+        style={styles}
+        width="27em"
+        height="27em"
+        strokeWidth={4}
+        strokeColor="blue" /></>
   );
 };
 
